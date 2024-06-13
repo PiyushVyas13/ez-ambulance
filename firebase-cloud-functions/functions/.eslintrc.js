002dcacbc1a21604fbb7ee8850704be160096ac9
Piyush Vyas <vyaspiyush2208@gmail.com>
2024-06-13 12:23:23 +0530

    deployed functions addEmployee, deleteEmployee

--- firebase-cloud-functions/functions/.eslintrc.js
+++ firebase-cloud-functions/functions/.eslintrc.js
@@ -1,28 +1,32 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true,
-  },
-  parserOptions: {
-    "ecmaVersion": 2018,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    "no-restricted-globals": ["error", "name", "length"],
-    "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
-  },
-  overrides: [
-    {
-      files: ["**/*.spec.*"],
-      env: {
-        mocha: true,
-      },
-      rules: {},
+    env: {
+        es6: true,
+        node: true,
     },
-  ],
-  globals: {},
+    parserOptions: {
+        "ecmaVersion": 2018,
+    },
+    extends: [
+        "eslint:recommended",
+        "google",
+    ],
+    rules: {
+        "no-restricted-globals": ["error", "name", "length"],
+        "prefer-arrow-callback": "error",
+        "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+        "indent": [
+            "error", 4,
+        ],
+        "max-len": ["error", 150],
+    },
+    overrides: [
+        {
+            files: ["**/*.spec.*"],
+            env: {
+                mocha: true,
+            },
+            rules: {},
+        },
+    ],
+    globals: {},
 };
--- firebase-cloud-functions/functions/index.js
+++ firebase-cloud-functions/functions/index.js
@@ -7,12 +7,10 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-const {onDocumentDeleted, onDocumentCreated} = require("firebase-functions/v2/firestore")
+const {onDocumentDeleted, onDocumentCreated} = require("firebase-functions/v2/firestore");
 const {initializeApp, applicationDefault} = require("firebase-admin/app");
-const { credential } = require("firebase-admin");
-const {getAuth} = require("firebase-admin/auth")
+const {getAuth} = require("firebase-admin/auth");
 const {getFirestore} = require("firebase-admin/firestore");
 const crypto = require("crypto");
 
@@ -25,57 +23,54 @@ const crypto = require("crypto");
 // });
 
 const app = initializeApp({
-    credential: applicationDefault()
+    credential: applicationDefault(),
 });
-const db = getFirestore()
-
+const db = getFirestore();
 
 
 exports.deleteEmployee = onDocumentDeleted("/owners/{ownerId}/employees/{employeeMail}", (event) => {
     const snap = event.data;
 
-    if(!snap) {
+    if (!snap) {
         return logger.error("No data associated with the snapshot.");
     }
     const data = snap.data();
 
     const userId = data.user_id;
 
-    if(!userId) {
+    if (!userId) {
         return logger.error("User id field did not exist in the document");
     }
 
     logger.log("Beggining deletion operation");
     return getAuth(app).deleteUser(userId)
-            .then(() => {
-                return logger.log('Deleted user account' + userId);
-                
-            })
-            .catch((error) => {
-                return logger.error('Unable to delete account' + error);
-            })
-            .finally(() => {
-                logger.log("finished function");
-            })
-
-})
+        .then(() => {
+            return logger.log("Deleted user account" + userId);
+        })
+        .catch((error) => {
+            return logger.error("Unable to delete account" + error);
+        })
+        .finally(() => {
+            logger.log("finished function");
+        });
+});
 
 const generateDummyPassword = (length) => {
     const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-    let password = ""
+    let password = "";
 
-    for(let i=0; i<length; i++) {
-        const index = crypto.randomInt(chars.length)
-        password += chars.charAt(index)
+    for (let i=0; i<length; i++) {
+        const index = crypto.randomInt(chars.length);
+        password += chars.charAt(index);
     }
 
     return password;
-}
+};
 
 exports.addEmployee = onDocumentCreated("/owners/{ownerId}/employees/{employeeMail}", (event) => {
     const snap = event.data;
 
-    if(!snap) {
+    if (!snap) {
         return logger.error("No data associated with the snapshot");
     }
 
@@ -83,33 +78,31 @@ exports.addEmployee = onDocumentCreated("/owners/{ownerId}/employees/{employeeMa
     const employeeEmail = event.params.employeeMail;
     const ownerId = event.params.ownerId;
 
-    logger.log("Creating User...")
+    logger.log("Creating User...");
     return getAuth(app).createUser({
         email: employeeEmail,
         password: generateDummyPassword(6),
         phoneNumber: data.phone_number,
         displayName: data.name,
     })
-    .then((userRecord) => {
-        logger.log("Successfully created new user", userRecord.uid)
-        logger.log("Starting update operation...")
-        return db.collection("owners").doc(ownerId).collection("employees").doc(employeeEmail).update({user_id: userRecord.uid})
+        .then((userRecord) => {
+            logger.log("Successfully created new user", userRecord.uid);
+            logger.log("Starting update operation...");
+            return db.collection("owners").doc(ownerId).collection("employees").doc(employeeEmail).update({user_id: userRecord.uid})
                 .then((result) => {
-                    return logger.log("userId added to employee at:", result.writeTime)
+                    return logger.log("userId added to employee at:", result.writeTime);
                 })
                 .catch((error) => {
-                    return logger.error("cannot update document", error)
+                    return logger.error("cannot update document", error);
                 })
                 .finally(() => {
                     return logger.log("finished update document operation");
-                })
-
-
-    })
-    .catch((error) => {
-        return logger.log("Cannot create user", error)
-    })
-    .finally(() => {
-        return logger.log("Finished addEmployee")
-    })
-})
+                });
+        })
+        .catch((error) => {
+            return logger.log("Cannot create user", error);
+        })
+        .finally(() => {
+            return logger.log("Finished addEmployee");
+        });
+});
