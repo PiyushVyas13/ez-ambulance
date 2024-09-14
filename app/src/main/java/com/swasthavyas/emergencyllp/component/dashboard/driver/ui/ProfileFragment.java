173eecd35b814412b94b1dcba7df9f74f5ce2f4e
Piyush Vyas <vyaspiyush2208@gmail.com>
2024-09-15 03:03:50 +0530

    feat: profile pic uploads for driver

--- app/src/main/java/com/swasthavyas/emergencyllp/component/dashboard/driver/ui/ProfileFragment.java
+++ app/src/main/java/com/swasthavyas/emergencyllp/component/dashboard/driver/ui/ProfileFragment.java
@@ -3,15 +3,27 @@ package com.swasthavyas.emergencyllp.component.dashboard.driver.ui;
 import static com.swasthavyas.emergencyllp.util.AppConstants.TAG;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -19,6 +31,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.swasthavyas.emergencyllp.AuthActivity;
 import com.swasthavyas.emergencyllp.R;
 import com.swasthavyas.emergencyllp.component.dashboard.driver.viewmodel.EmployeeViewModel;
+import com.swasthavyas.emergencyllp.component.dashboard.driver.worker.DriverProfileUpdateWorker;
 import com.swasthavyas.emergencyllp.component.dashboard.ui.DriverDashboardFragment;
 import com.swasthavyas.emergencyllp.component.trip.ui.TripActivity;
 import com.swasthavyas.emergencyllp.databinding.FragmentProfileDriverBinding;
@@ -33,6 +46,8 @@ public class ProfileFragment extends Fragment {
     FragmentProfileDriverBinding viewBinding;
     EmployeeViewModel employeeViewModel;
 
+    ActivityResultLauncher<PickVisualMediaRequest> uploadProfilePicRequest;
+
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -41,8 +56,41 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        uploadProfilePicRequest = registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+            if(uri != null) {
+                Drawable currentDrawable = viewBinding.profileImage.getDrawable();
+                viewBinding.profileImage.setImageURI(uri);
+                uploadProfileImage(currentDrawable, uri);
+            }
+        });
         employeeViewModel = new ViewModelProvider(requireActivity()).get(EmployeeViewModel.class);
+    }
+
+
+
+    private void uploadProfileImage(Drawable currentDrawable, Uri newProfilePicUri) {
+        Data inputData = new Data.Builder()
+                .putString("profile_uri_string", newProfilePicUri.toString())
+                .build();
+
+        OneTimeWorkRequest profileUploadRequest = new OneTimeWorkRequest.Builder(DriverProfileUpdateWorker.class)
+                .setInputData(inputData)
+                .setConstraints(new Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build())
+                .build();
+
+        WorkManager.getInstance(requireContext()).enqueue(profileUploadRequest);
+
+        WorkManager.getInstance(requireContext())
+                .getWorkInfoByIdLiveData(profileUploadRequest.getId())
+                .observe(getViewLifecycleOwner(), workInfo -> {
+                    if(workInfo.getState().isFinished() && workInfo.getState() == WorkInfo.State.SUCCEEDED) {
+                        Toast.makeText(requireContext(), "Profile Successfully updated!", Toast.LENGTH_SHORT).show();
+                    } else if(workInfo.getState().isFinished() && workInfo.getState() == WorkInfo.State.FAILED){
+                        Toast.makeText(requireContext(), "Something Went wrong!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, "uploadProfileImage: " + workInfo.getOutputData().getString("message"));
+                        viewBinding.profileImage.setImageDrawable(currentDrawable);
+                    }
+                });
     }
 
     @Override
@@ -50,6 +98,8 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         viewBinding = FragmentProfileDriverBinding.inflate(getLayoutInflater());
         FirebaseDatabase database = FirebaseService.getInstance().getDatabaseInstance();
+
+
 
         employeeViewModel.getCurrentEmployee().observe(getViewLifecycleOwner(), employeeDriver -> {
             Log.d(TAG, "onCreateView: Profile fragment observer triggered");
@@ -61,6 +111,11 @@ public class ProfileFragment extends Fragment {
             viewBinding.driverAge.setText(String.format(Locale.ENGLISH, "%d", employeeDriver.getAge()));
             viewBinding.driverAadhaar.setText(employeeDriver.getAadhaarNumber());
 
+            viewBinding.changeProfileImage.setOnClickListener(v -> {
+                uploadProfilePicRequest.launch(new PickVisualMediaRequest.Builder()
+                        .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
+                        .build());
+            });
 
             viewBinding.signOutBtn.setOnClickListener(v -> {
                 DatabaseReference activeDriverReference = database
---
+++ app/src/main/java/com/swasthavyas/emergencyllp/component/dashboard/driver/worker/DriverProfileUpdateWorker.java
@@ -0,0 +1,67 @@
+package com.swasthavyas.emergencyllp.component.dashboard.driver.worker;
+
+import android.content.Context;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.work.WorkerParameters;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.swasthavyas.emergencyllp.util.asyncwork.ListenableWorkerAdapter;
+import com.swasthavyas.emergencyllp.util.asyncwork.NetworkResultCallback;
+import com.swasthavyas.emergencyllp.util.firebase.FirebaseService;
+
+public class DriverProfileUpdateWorker extends ListenableWorkerAdapter {
+
+    public DriverProfileUpdateWorker(@NonNull Context appContext, @NonNull WorkerParameters workerParams) {
+        super(appContext, workerParams);
+    }
+
+    @Override
+    public void doAsyncBackgroundTask(NetworkResultCallback callback) {
+        String profileUriString = getInputData().getString("profile_uri_string");
+
+        if(profileUriString == null) {
+            callback.onFailure(new IllegalArgumentException("Profile Uri not provided"));
+            return;
+        }
+
+        FirebaseUser currentUser = FirebaseService.getInstance().getAuthInstance().getCurrentUser();
+
+        if(currentUser == null) {
+            callback.onFailure(new IllegalArgumentException("Unauthorized!"));
+            return;
+        }
+
+        StorageReference rootRef = FirebaseService.getInstance().getStorageInstance().getReference();
+
+        StorageReference profileRef = rootRef.child(String.format("/users/employees/%s/profile_image.jpg", currentUser.getEmail()));
+
+        Uri profileUri = Uri.parse(profileUriString);
+
+        profileRef.putFile(profileUri)
+                .addOnCompleteListener(task -> {
+                   if(task.isSuccessful()) {
+                       FirebaseFirestore dbInstance = FirebaseService.getInstance().getFirestoreInstance();
+
+                       dbInstance
+                               .collection("employees")
+                               .document(currentUser.getEmail())
+                               .update("profileImageRef", profileRef.toString())
+                               .addOnCompleteListener(updateTask -> {
+                                  if(updateTask.isSuccessful()) {
+                                      callback.onSuccess(null);
+                                  } else {
+                                      callback.onFailure(updateTask.getException());
+                                  }
+                               });
+                   } else {
+                       callback.onFailure(task.getException());
+                   }
+                });
+    }
+}
